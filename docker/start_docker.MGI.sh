3c57c05293be9e1892b9b3157524611297efbe9a
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2020-06-11 16:42:37 -0500

    Adding WUDocker

--- docker/start_docker.MGI.sh
+++
@@ -1,3 +0,0 @@
-source docker_image.sh
-bsub -Is -q research-hpc -a "docker($IMAGE)" /bin/bash
-
--- docker/start_docker.sh
+++
@@ -1,6 +0,0 @@
-# Basic script to start docker container
-
-source docker_image.sh
-
-# Starting with no mounted volumes:
-docker run -it $IMAGE

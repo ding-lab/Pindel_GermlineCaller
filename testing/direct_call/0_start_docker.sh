99321782171b19b623f26a0319ac0b930818c60e
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-06-12 12:12:35 -0500

    direct testing update

--- testing/direct_call/0_start_docker.sh
+++ testing/direct_call/0_start_docker.sh
@@ -5,16 +5,17 @@
 source ../../docker/docker_image.sh
 
 # katmai
-#VEP_ROOT="/diskmnt/Datasets/VEP"
-#REF_ROOT="/diskmnt/Datasets/Reference"
+VEP_ROOT="/diskmnt/Datasets/VEP"
+REF_ROOT="/diskmnt/Datasets/Reference"
 #DATA_ROOT="/home/mwyczalk_test/Projects/GermlineCaller/C3L-00001"
+DATA_ROOT="/home/mwyczalk_test/Projects/GermlineCaller/TinJasmine/submodules/Pindel_GermlineCaller/testing/demo_data"
 
 # MGI
-VEP_ROOT="/gscmnt/gc7202/dinglab/common/databases/VEP"
-REF_ROOT="/gscmnt/gc7202/dinglab/common/Reference/A_Reference"
-#DATA_ROOT="/gscmnt/gc2508/dinglab/mwyczalk/GermlineCaller.Testing/C3L-00001"
-DATA_ROOT="/gscmnt/gc2541/cptac3_analysis/cromwell-workdir/cromwell-executions/TinJasmine.cwl"
-ARGS="-M MGI -P"
+#VEP_ROOT="/gscmnt/gc7202/dinglab/common/databases/VEP"
+#REF_ROOT="/gscmnt/gc7202/dinglab/common/Reference/A_Reference"
+##DATA_ROOT="/gscmnt/gc2508/dinglab/mwyczalk/GermlineCaller.Testing/C3L-00001"
+#DATA_ROOT="/gscmnt/gc2541/cptac3_analysis/cromwell-workdir/cromwell-executions/TinJasmine.cwl"
+#ARGS="-M MGI -P"
 
 MOUNT="$VEP_ROOT:/vep"
 MOUNT="$MOUNT $REF_ROOT:/ref"

97ef611c15a217b20b7b3f562335bd1fef455e49
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2020-06-13 12:18:47 -0500

    Implementing simplified CWL testing for pindel_caller
    * Testing C3L-00081

--- testing/cwl_call-MGI/1_run_pindel_caller_parallel.sh
+++ testing/cwl_call-MGI/1_run_pindel_caller_parallel.sh
@@ -5,8 +5,8 @@ source /opt/lsf9/conf/lsf.conf
 
 CONFIG="cromwell-config-db.dat"
 
-CWL="../../cwl/vep_annotate.cwl"
-YAML="cwl-yaml/vep_annotate-C3L-00001.yaml"
+CWL="../../cwl/pindel_caller.cwl"
+YAML="cwl-yaml/pindel_caller.C3L-00081.yaml"
 
 
 CROMWELL="/usr/local/cromwell/cromwell-47.jar"
---
+++ testing/cwl_call-MGI/README.md
@@ -0,0 +1,4 @@
+Testing of C3L-00081 on MGI
+
+Parameters based on 
+/gscuser/mwyczalk/projects/GermlineCaller/TinJasmine/testing/README.C3L-00081.md
